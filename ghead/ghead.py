3f33ad777eceb50f6976ea1498a574025788ffbd
Nathan Bjork <nbjork@gmail.com>
2018-05-09 14:22:18 -0500

    Update ghead.py

--- ghead/ghead.py
+++ ghead/ghead.py
@@ -1,5 +1,6 @@
-
 from discord.ext import commands
+from bs4 import BeautifulSoup
+import requests
 
 class Ghead:
     def __init__(self, bot):
@@ -7,8 +8,21 @@ class Ghead:
 
     @commands.command()
     async def ghead(self, head):
+        
+        url = 'http://whiskeycrow.com/Graal/heads/'
+        ext = 'png'
 
-        await self.bot.say("http://whiskeycrow.com/Graal/heads/" + head + ".png")
+        def list_dir(url, ext=''):
+            page = requests.get(url).text
+            soup = BeautifulSoup(page, 'html.parser')
+            return [node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+
+        for file in list_dir(url, ext):
+            if head + ".png" == file:
+                await self.bot.say("http://whiskeycrow.com/Graal/heads/" + head + ".png")
+            else:
+                await self.bot.say("Not found, noob")
+
 
 def setup(bot):
     bot.add_cog(Ghead(bot))
