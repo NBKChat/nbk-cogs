ee6598600655ad0961ef258c7fefd097c4d0f222
johnpz <j.john.pease@gmail.com>
2018-05-09 20:19:09 -0400

    added script to convert sprites to gif added readme

---
+++ animate-sprite-heads.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+convertgif() {
+    convert -dispose 3 -delay 20 -loop 0  ${1}.png -crop 32x32 +repage -depth 4 ${1}.tmp.gif
+    convert ${1}.tmp.gif[0-3] ${1}.gif
+    rm -f ${1}.tmp.gif
+}
+
+for i in $(find ./ -maxdepth 1 -name "*.png" | sed s/\.png//g); do
+    while [ $(jobs | wc -l) -ge 12 ]; do
+        sleep 1
+    done
+    convertgif ${i} &
+done
