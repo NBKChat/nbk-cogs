98b1b7d0b542884e1a3d63112eb45fee1d892371
johnpz <j.john.pease@gmail.com>
2018-08-11 18:50:02 -0400

    stuf

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -7,16 +7,15 @@ class countdown:
     def __init__(self, bot):
         self.bot = bot
         self.jamtime = datetime.datetime(2018, 10, 29)
-        self.output = "{} days {} hours {} minutes until the jam".format(self.delta.days, self.delta.seconds//3600, (self.delta.seconds//60)%60)
 
-    def countdown(self):
+    def countdownout(self):
         delta = self.jamtime - datetime.datetime.now()
         return "{} days {} hours {} minutes until the jam".format(delta.days, delta.seconds//3600, (delta.seconds//60)%60)
 
     @commands.command(name="jamcountdown", aliases=["jamcount", "jamtimer", "jamtime"])
 
     async def countdown(self):
-        await self.bot.say(self.output)
+        await self.bot.say(countdownout())
 
 
 def setup(bot):
