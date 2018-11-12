ae254d7238fca30f4fd0338e4ff873df792ee0c7
John Pease <jaypz@users.noreply.github.com>
2018-11-12 13:10:53 -0500

    Update countdown.py

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -6,14 +6,14 @@ class countdown:
 
     def __init__(self, bot):
         self.bot = bot
-        self.jamtime = datetime.datetime(2018, 10, 26)
+        self.jamtime = datetime.datetime(2019, 03, 01)
 
     @commands.command(name="jamcountdown", aliases=["jamcount", "jamtimer", "jamtime"])
 
     async def countdown(self):
         delta = self.jamtime - datetime.datetime.now()
         if delta.days < 0:
-            output = "its jamtime"
+            output = "its jamtime decision time"
         else:
             output =  "{} days {} hours {} minutes until the jam".format(delta.days, delta.seconds//3600, (delta.seconds//60)%60)
         await self.bot.say(output)
