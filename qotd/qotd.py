cc8eade25c7d070cb6eedefc9bb8e14256fc5e03
Nathan Bjork <nbjork@gmail.com>
2019-02-02 16:59:14 -0600

    Update qotd.py

--- qotd/qotd.py
+++ qotd/qotd.py
@@ -1,8 +1,6 @@
 from discord.ext import commands
-import wikiquote
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
+import requests
+import json
 
 class Qotd:
 
@@ -12,9 +10,14 @@ class Qotd:
     @commands.command()
 
     async def qotd(self):
-        qotd = wikiquote.quote_of_the_day()
-        await self.bot.say(qotd[0])
-        await self.bot.say("- " + qotd[1])
+        response = requests.get("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json")
+        responseJson = json.loads(response.text)
+        author = responseJson['quoteAuthor']
+        quote = responseJson['quoteText']
+        if author == "":
+            author = "Anonymous"
+        await self.bot.say(quote)
+        await self.bot.say("- " + author)
 
 def setup(bot):
     bot.add_cog(Qotd(bot))
