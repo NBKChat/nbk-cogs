7c04625f79dd7eb005642c9acc26b7c8ced63e1e
John Pease <jaypz@users.noreply.github.com>
2019-05-22 22:59:50 -0400

    shark week

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -6,16 +6,16 @@ class countdown:
 
     def __init__(self, bot):
         self.bot = bot
-        self.jamtime = datetime.datetime(2019, 3, 1)
+        self.jamtime = datetime.datetime(2019, 6, 15)
 
-    @commands.command(name="jamcountdown", aliases=["jamcount", "jamtimer", "jamtime"])
+    @commands.command(name="jamcountdown", aliases=["jamcount", "jamtimer", "jamtime", "sharkweek"])
 
     async def countdown(self):
         delta = self.jamtime - datetime.datetime.now()
         if delta.days < 0:
             output = "its jamtime decision time"
         else:
-            output =  "{} days {} hours {} minutes until the jam decision".format(delta.days, delta.seconds//3600, (delta.seconds//60)%60)
+            output =  "{} days {} hours {} minutes until shark week".format(delta.days, delta.seconds//3600, (delta.seconds//60)%60)
         await self.bot.say(output)
 
 
